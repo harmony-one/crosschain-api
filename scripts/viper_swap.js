fb66fc0f8b547f80a4f53e392a6421d54cad3ad0
Boris Polania <boris.polania@gmail.com>
2021-08-27 14:31:37 -0700

    Update viper_swap.js
    Comments Added
    Code cleaning

--- scripts/viper_swap.js
+++ scripts/viper_swap.js
@@ -1,79 +1,82 @@
-require("dotenv").config();
-const {
-  BridgeSDK,
-  TOKEN,
-  EXCHANGE_MODE,
-  NETWORK_TYPE,
-  ACTION_TYPE,
-} = require("bridge-sdk");
-const BN = require("bn.js");
-const Web3 = require("web3");
-const web3 = new Web3(
-  new Web3.providers.HttpProvider(process.env.HARMONY_NODE_URL)
-);
-const configs = require("bridge-sdk/lib/configs")
+const BN = require("bn.js")
+const Web3 = require("web3")
+const https = require('https')
 var contracts = require('./contracts.js')
 const ChainId = require('@venomswap/sdk').ChainId
+const web3 = new Web3(
+  new Web3.providers.HttpProvider(process.env.HARMONY_NODE_URL)
+)
 
-async function perform(amount, fromAddress, destinationAddress, routerContract, fromTokenContract, toTokenContract) {
-  const https = require('https')
-
-    const data = new TextEncoder().encode(
-        JSON.stringify({
-            "amount" : amount,
-            "oneAddress" : oneAddress,
-            "ethAddress" : ethAddress,
-            "destinationAddress" : ethAddress,
-            "routerContract" : routerContract,
-            "fromTokenContract" = fromTokenContract,
-            "toTokenContract" = toTokenContract
-        })
-    )
-
-    const options = {
+/* Receives all the required information to send a POST request
+ * to the API's `/swap/viper` endpoint
+ * @param {BigNumber} amount
+ * @param {string} oneAddress
+ * @param {string} ethAddress
+ * @param {Contract} routerContract
+ * @param {string} fromTokenContract
+ * @param {string} toTokenContract
+*/
+async function perform(amount, oneAddress, ethAddress, routerContract, fromTokenContract, toTokenContract) {
+  // Create the body of the POST request
+  const data = new TextEncoder().encode(
+    JSON.stringify({
+      "amount" : amount,
+      "oneAddress" : oneAddress,
+      "ethAddress" : ethAddress,
+      "destinationAddress" : ethAddress,
+      "routerContract" : routerContract,
+      "fromTokenContract" : fromTokenContract,
+      "toTokenContract" : toTokenContract
+    })
+  )
+  // set the option for the POST request
+  const options = {
     hostname: 'localhost',
     port: 3000,
     path: '/swap/bridge-in',
     method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': data.length
-        }
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length
     }
-
-    const req = https.request(options, res => {
-    console.log(`statusCode: ${res.statusCode}`)
-        res.on('data', d => {
-            process.stdout.write(d)
-        })
+  }
+  // send the request and deal with the possible responses
+  const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`)
+    res.on('data', d => {
+      process.stdout.write(d)
     })
-
-    req.on('error', error => {
-        console.error(error)
-    })
-
-    req.write(data)
-    req.end()
+  })
+  req.on('error', error => {
+    console.error(error)
+  })
+  req.write(data)
+  req.end()
 }
 
+/* Generate the transaction hashes and send the
+ * POST request to the cross-chain API
+*/
 async function main() {
   try {
+    // Set initial values
     const fromAddress = "Your From Address"
-    const ethAddress = "Your To Address"
+    const toAddress = "Your To Address"
     let amount = web3.utils.toWei("1", "ether")
     const destinationAddress = ethAddress
+    // get the Viper contracts
     const routerContract = contracts.getRouterContract(ChainId.HARMONY_TESTNET, wallet)
-    const fromTokenContract = await contracts.getTokenContract(ChainId.HARMONY_TESTNET, fromToken, wallet)
-    const toTokenContract = await contracts.getTokenContract(ChainId.HARMONY_TESTNET, toToken, wallet)
-    
+    const fromTokenContract = await contracts.getTokenContract(fromAddress, wallet)
+    const toTokenContract = await contracts.getTokenContract(toAddress, wallet)
+    // send the transaction to the API
     setTimeout(() => {
-      perform(amount, fromAddress, destinationAddress, routerContract, fromTokenContract, toTokenContract).then(() => {
-        console.log("done");
-      });
-    }, 10000);
+      await perform(amount, fromAddress, toAddress, routerContract, fromTokenContract, toTokenContract).then(() => {
+        console.log("done")
+      })
+    }, 10000)
   } catch (e) {
-    console.error("Error: ", e.message, e.response?.body);
+    console.error("Error: ", e.message, e.response?.body)
   }
 }
 
-main().then(() => {});
+main().then(() => {})
